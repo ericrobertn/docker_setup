2a52b28647e60269d032de83c3a2785876a4b561
Eric Neudorfer <ericneudorfer@gmail.com>
2020-11-05 17:55:12 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-sudo yum remove -y docker \
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
                   docker-common \
-                  docker-selinux \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
                   docker-engine
-				  
-				  
+
+sudo yum update -y 
 sudo yum install -y yum-utils \
   device-mapper-persistent-data \
   lvm2
@@ -11,14 +15,13 @@ sudo yum install -y yum-utils \
 sudo yum-config-manager \
 --add-repo \
 https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io --nobest
 
-curl https://releases.rancher.com/install-docker/17.06.sh | sh
-	
 
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean |
