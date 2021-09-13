cee52584f231f875faf1376528d82812055f5f2b
Eric Neudorfer <ericneudorfer@gmail.com>
2021-09-12 21:20:46 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,7 +21,7 @@ sudo yum install docker-ce docker-ce-cli containerd.io --nobest
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 curl -s https://raw.githubusercontent.com/ZZROTDesign/docker-clean/v2.0.4/docker-clean |
